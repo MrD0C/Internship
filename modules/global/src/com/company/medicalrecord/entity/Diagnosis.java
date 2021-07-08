861883fd7248d4a940c361b3d2a6b887bc847994
Daniil Kudiyarov <daniilkudiyarovsocial@gmail.com>
2021-07-08 15:16:47 +0400

    Add Diagnosis embedded class

---
+++ modules/global/src/com/company/medicalrecord/entity/Diagnosis.java
@@ -0,0 +1,37 @@
+package com.company.medicalrecord.entity;
+
+import com.haulmont.chile.core.annotations.MetaClass;
+import com.haulmont.chile.core.annotations.NamePattern;
+import com.haulmont.cuba.core.entity.EmbeddableEntity;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@MetaClass(name = "medicalrecord_Diagnosis")
+@Embeddable
+@NamePattern("%s|name")
+public class Diagnosis extends EmbeddableEntity {
+    private static final long serialVersionUID = 1523421526646234626L;
+
+    @Column(name = "ICD10_NAME")
+    private String icd10Name;
+
+    @Column(name = "NAME")
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIcd10Name() {
+        return icd10Name;
+    }
+
+    public void setIcd10Name(String icd10Name) {
+        this.icd10Name = icd10Name;
+    }
+}
