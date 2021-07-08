1e6952af832f6f501d5e478349361af1bd0f4a29
Daniil Kudiyarov <daniilkudiyarovsocial@gmail.com>
2021-07-08 15:24:55 +0400

    Add Medication embedded class

---
+++ modules/global/src/com/company/medicalrecord/entity/Medication.java
@@ -0,0 +1,37 @@
+package com.company.medicalrecord.entity;
+
+import com.haulmont.chile.core.annotations.MetaClass;
+import com.haulmont.cuba.core.entity.EmbeddableEntity;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.Lob;
+
+@MetaClass(name = "medicalrecord_Medication")
+@Embeddable
+public class Medication extends EmbeddableEntity {
+    private static final long serialVersionUID = -4727025595642704352L;
+
+    @Column(name = "MEDICINE_NAME")
+    private String medicineName;
+
+    @Lob
+    @Column(name = "INTAKE_RECOMMENDATION")
+    private String intakeRecommendation;
+
+    public String getIntakeRecommendation() {
+        return intakeRecommendation;
+    }
+
+    public void setIntakeRecommendation(String intakeRecommendation) {
+        this.intakeRecommendation = intakeRecommendation;
+    }
+
+    public String getMedicineName() {
+        return medicineName;
+    }
+
+    public void setMedicineName(String medicineName) {
+        this.medicineName = medicineName;
+    }
+}
