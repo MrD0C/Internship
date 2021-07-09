9c541d7cb5f40af1b42f353f1e3bc998e930292b
Daniil Kudiyarov <daniilkudiyarovsocial@gmail.com>
2021-07-09 15:08:40 +0400

    Added base values to WeightMonitoring onInitEntity event

--- modules/web/src/com/company/medicalrecord/web/screens/weightmonitoring/WeightMonitoringEdit.java
+++ modules/web/src/com/company/medicalrecord/web/screens/weightmonitoring/WeightMonitoringEdit.java
@@ -1,11 +1,21 @@
 package com.company.medicalrecord.web.screens.weightmonitoring;
 
+import com.company.medicalrecord.entity.unit.UnitMass;
 import com.haulmont.cuba.gui.screen.*;
 import com.company.medicalrecord.entity.monitoring.WeightMonitoring;
+
+import java.time.LocalDateTime;
 
 @UiController("medicalrecord_WeightMonitoring.edit")
 @UiDescriptor("weight-monitoring-edit.xml")
 @EditedEntityContainer("weightMonitoringDc")
 @LoadDataBeforeShow
 public class WeightMonitoringEdit extends StandardEditor<WeightMonitoring> {
+    @Subscribe
+    public void onInitEntity(InitEntityEvent<WeightMonitoring> event) {
+        event.getEntity().setLocalDateTime(LocalDateTime.now());
+        event.getEntity().setUnit(UnitMass.KILOGRAMS);
+    }
+
+
 }
