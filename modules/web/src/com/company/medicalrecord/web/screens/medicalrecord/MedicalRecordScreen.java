ab8bf72f8fdd997de3047ce0f9aa280d9a1507f2
Daniil Kudiyarov <daniilkudiyarovsocial@gmail.com>
2021-07-12 16:06:26 +0400

    Added notification to MedicalRecordScreen to tab Note

--- modules/web/src/com/company/medicalrecord/web/screens/medicalrecord/MedicalRecordScreen.java
+++ modules/web/src/com/company/medicalrecord/web/screens/medicalrecord/MedicalRecordScreen.java
@@ -1,13 +1,12 @@
 package com.company.medicalrecord.web.screens.medicalrecord;
 
 import com.company.medicalrecord.entity.record.NoteRecord;
+import com.haulmont.cuba.gui.Notifications;
+import com.haulmont.cuba.gui.RemoveOperation;
 import com.haulmont.cuba.gui.actions.list.ViewAction;
 import com.haulmont.cuba.gui.components.DataGrid;
 import com.haulmont.cuba.gui.model.CollectionLoader;
-import com.haulmont.cuba.gui.screen.Screen;
-import com.haulmont.cuba.gui.screen.Subscribe;
-import com.haulmont.cuba.gui.screen.UiController;
-import com.haulmont.cuba.gui.screen.UiDescriptor;
+import com.haulmont.cuba.gui.screen.*;
 
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -21,6 +20,8 @@ public class MedicalRecordScreen extends Screen {
     private DataGrid<NoteRecord> noteRecordsTable;
     @Named("noteRecordsTable.view")
     private ViewAction<NoteRecord> noteRecordsTableView;
+    @Inject
+    private Notifications notifications;
 
     @Subscribe
     public void onInit(InitEvent event) {
@@ -28,5 +29,28 @@ public class MedicalRecordScreen extends Screen {
         noteRecordsTable.setItemClickAction(noteRecordsTableView);
     }
 
+    @Install(to = "noteRecordsTable.create", subject = "afterCommitHandler")
+    private void noteRecordsTableCreateAfterCommitHandler(NoteRecord noteRecord) {
+        createTraySuccessNotification("Note saved");
+    }
+
+    @Install(to = "noteRecordsTable.edit", subject = "afterCommitHandler")
+    private void noteRecordsTableEditAfterCommitHandler(NoteRecord noteRecord) {
+        createTraySuccessNotification("Note changes saved");
+    }
+
+    @Install(to = "noteRecordsTable.remove", subject = "afterActionPerformedHandler")
+    private void noteRecordsTableRemoveAfterActionPerformedHandler(RemoveOperation.AfterActionPerformedEvent<NoteRecord> afterActionPerformedEvent) {
+        createTraySuccessNotification("Note deleted");
+    }
+
+    private void createTraySuccessNotification(String description){
+        notifications.create()
+                .withCaption("Success")
+                .withDescription(description)
+                .withType(Notifications.NotificationType.TRAY)
+                .withPosition(Notifications.Position.BOTTOM_RIGHT)
+                .show();
+    }
 
 }
