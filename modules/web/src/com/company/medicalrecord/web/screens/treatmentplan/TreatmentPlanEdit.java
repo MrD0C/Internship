125cf304d5cc346dfb7e0f1c8c868dc832cc452f
Daniil Kudiyarov <daniilkudiyarovsocial@gmail.com>
2021-07-12 15:41:14 +0400

    Completed treatment-plan-edit.xml UI, changed Dialog Mode settings

--- modules/web/src/com/company/medicalrecord/web/screens/treatmentplan/TreatmentPlanEdit.java
+++ modules/web/src/com/company/medicalrecord/web/screens/treatmentplan/TreatmentPlanEdit.java
@@ -3,9 +3,17 @@ package com.company.medicalrecord.web.screens.treatmentplan;
 import com.haulmont.cuba.gui.screen.*;
 import com.company.medicalrecord.entity.TreatmentPlan;
 
+import java.time.LocalDateTime;
+
 @UiController("medicalrecord_TreatmentPlan.edit")
 @UiDescriptor("treatment-plan-edit.xml")
 @EditedEntityContainer("treatmentPlanDc")
 @LoadDataBeforeShow
 public class TreatmentPlanEdit extends StandardEditor<TreatmentPlan> {
+    @Subscribe
+    public void onInitEntity(InitEntityEvent<TreatmentPlan> event) {
+        event.getEntity().setDate(LocalDateTime.now());
+    }
+
+
 }
