c05e78af27ff8ce9dbe1d26d04d424ef679b4296
Daniil Kudiyarov <daniilkudiyarovsocial@gmail.com>
2021-07-15 10:17:25 +0400

    Deleted unnecessary scripts

--- modules/core/db/init/hsql/10.create-db.sql
+++ modules/core/db/init/hsql/10.create-db.sql
@@ -1,65 +1,3 @@
--- begin MEDICALRECORD_CONSULTATION_RECORD
-create table MEDICALRECORD_CONSULTATION_RECORD (
-    ID varchar(36) not null,
-    VERSION integer not null,
-    CREATE_TS timestamp,
-    CREATED_BY varchar(50),
-    UPDATE_TS timestamp,
-    UPDATED_BY varchar(50),
-    DELETE_TS timestamp,
-    DELETED_BY varchar(50),
-    --
-    DIAGNOSIS_ICD10_NAME varchar(255),
-    DIAGNOSIS_NAME varchar(255),
-    CREATION_INFO_CLINIC_NAME longvarchar,
-    CREATION_INFO_DOCTOR_SURNAME varchar(255),
-    CREATION_INFO_DATE_ timestamp not null,
-    DESCRIPTION_COMPLAINTS longvarchar,
-    DESCRIPTION_ANAMNESIS longvarchar,
-    DESCRIPTION_OBJECTIVELY longvarchar,
-    DESCRIPTION_CONCLUSION longvarchar,
-    --
-    primary key (ID)
-)^
--- end MEDICALRECORD_CONSULTATION_RECORD
--- begin MEDICALRECORD_TREATMENT_PLAN
-create table MEDICALRECORD_TREATMENT_PLAN (
-    ID varchar(36) not null,
-    VERSION integer not null,
-    CREATE_TS timestamp,
-    CREATED_BY varchar(50),
-    UPDATE_TS timestamp,
-    UPDATED_BY varchar(50),
-    DELETE_TS timestamp,
-    DELETED_BY varchar(50),
-    --
-    TREATMENT_TYPE varchar(50) not null,
-    TITLE longvarchar not null,
-    NAME longvarchar,
-    DATE_ timestamp not null,
-    EXAMINATION_RECORD_ID varchar(36) not null,
-    --
-    primary key (ID)
-)^
--- end MEDICALRECORD_TREATMENT_PLAN
--- begin MEDICALRECORD_MEDICATION
-create table MEDICALRECORD_MEDICATION (
-    ID varchar(36) not null,
-    VERSION integer not null,
-    CREATE_TS timestamp,
-    CREATED_BY varchar(50),
-    UPDATE_TS timestamp,
-    UPDATED_BY varchar(50),
-    DELETE_TS timestamp,
-    DELETED_BY varchar(50),
-    --
-    MEDICINE_NAME varchar(255) not null,
-    INTAKE_RECOMMENDATION longvarchar,
-    CONSULTATION_RECORD_ID varchar(36) not null,
-    --
-    primary key (ID)
-)^
--- end MEDICALRECORD_MEDICATION
 -- begin MEDICALRECORD_WEIGHT_MONITORING
 create table MEDICALRECORD_WEIGHT_MONITORING (
     ID varchar(36) not null,
@@ -78,42 +16,6 @@ create table MEDICALRECORD_WEIGHT_MONITORING (
     primary key (ID)
 )^
 -- end MEDICALRECORD_WEIGHT_MONITORING
--- begin MEDICALRECORD_PULSE_MONITORING
-create table MEDICALRECORD_PULSE_MONITORING (
-    ID varchar(36) not null,
-    VERSION integer not null,
-    CREATE_TS timestamp,
-    CREATED_BY varchar(50),
-    UPDATE_TS timestamp,
-    UPDATED_BY varchar(50),
-    DELETE_TS timestamp,
-    DELETED_BY varchar(50),
-    --
-    VALUE_ double precision not null,
-    UNIT varchar(50) not null,
-    LOCAL_DATE_TIME timestamp not null,
-    --
-    primary key (ID)
-)^
--- end MEDICALRECORD_PULSE_MONITORING
--- begin MEDICALRECORD_TEMPERATURE_MONITORING
-create table MEDICALRECORD_TEMPERATURE_MONITORING (
-    ID varchar(36) not null,
-    VERSION integer not null,
-    CREATE_TS timestamp,
-    CREATED_BY varchar(50),
-    UPDATE_TS timestamp,
-    UPDATED_BY varchar(50),
-    DELETE_TS timestamp,
-    DELETED_BY varchar(50),
-    --
-    VALUE_ double precision not null,
-    UNIT varchar(50) not null,
-    LOCAL_DATE_TIME timestamp not null,
-    --
-    primary key (ID)
-)^
--- end MEDICALRECORD_TEMPERATURE_MONITORING
 -- begin MEDICALRECORD_EXAMINATION_RECORD
 create table MEDICALRECORD_EXAMINATION_RECORD (
     ID varchar(36) not null,
@@ -180,6 +82,24 @@ create table MEDICALRECORD_NOTE_RECORD (
     primary key (ID)
 )^
 -- end MEDICALRECORD_NOTE_RECORD
+-- begin MEDICALRECORD_PULSE_MONITORING
+create table MEDICALRECORD_PULSE_MONITORING (
+    ID varchar(36) not null,
+    VERSION integer not null,
+    CREATE_TS timestamp,
+    CREATED_BY varchar(50),
+    UPDATE_TS timestamp,
+    UPDATED_BY varchar(50),
+    DELETE_TS timestamp,
+    DELETED_BY varchar(50),
+    --
+    VALUE_ double precision not null,
+    UNIT varchar(50) not null,
+    LOCAL_DATE_TIME timestamp not null,
+    --
+    primary key (ID)
+)^
+-- end MEDICALRECORD_PULSE_MONITORING
 -- begin MEDICALRECORD_ANALYSIS_INDICATOR
 create table MEDICALRECORD_ANALYSIS_INDICATOR (
     ID varchar(36) not null,
@@ -201,3 +121,83 @@ create table MEDICALRECORD_ANALYSIS_INDICATOR (
     primary key (ID)
 )^
 -- end MEDICALRECORD_ANALYSIS_INDICATOR
+-- begin MEDICALRECORD_TREATMENT_PLAN
+create table MEDICALRECORD_TREATMENT_PLAN (
+    ID varchar(36) not null,
+    VERSION integer not null,
+    CREATE_TS timestamp,
+    CREATED_BY varchar(50),
+    UPDATE_TS timestamp,
+    UPDATED_BY varchar(50),
+    DELETE_TS timestamp,
+    DELETED_BY varchar(50),
+    --
+    TREATMENT_TYPE varchar(50) not null,
+    TITLE longvarchar not null,
+    NAME longvarchar,
+    DATE_ timestamp not null,
+    EXAMINATION_RECORD_ID varchar(36) not null,
+    --
+    primary key (ID)
+)^
+-- end MEDICALRECORD_TREATMENT_PLAN
+-- begin MEDICALRECORD_MEDICATION
+create table MEDICALRECORD_MEDICATION (
+    ID varchar(36) not null,
+    VERSION integer not null,
+    CREATE_TS timestamp,
+    CREATED_BY varchar(50),
+    UPDATE_TS timestamp,
+    UPDATED_BY varchar(50),
+    DELETE_TS timestamp,
+    DELETED_BY varchar(50),
+    --
+    MEDICINE_NAME varchar(255) not null,
+    INTAKE_RECOMMENDATION longvarchar,
+    CONSULTATION_RECORD_ID varchar(36) not null,
+    --
+    primary key (ID)
+)^
+-- end MEDICALRECORD_MEDICATION
+-- begin MEDICALRECORD_TEMPERATURE_MONITORING
+create table MEDICALRECORD_TEMPERATURE_MONITORING (
+    ID varchar(36) not null,
+    VERSION integer not null,
+    CREATE_TS timestamp,
+    CREATED_BY varchar(50),
+    UPDATE_TS timestamp,
+    UPDATED_BY varchar(50),
+    DELETE_TS timestamp,
+    DELETED_BY varchar(50),
+    --
+    VALUE_ double precision not null,
+    UNIT varchar(50) not null,
+    LOCAL_DATE_TIME timestamp not null,
+    --
+    primary key (ID)
+)^
+-- end MEDICALRECORD_TEMPERATURE_MONITORING
+-- begin MEDICALRECORD_CONSULTATION_RECORD
+create table MEDICALRECORD_CONSULTATION_RECORD (
+    ID varchar(36) not null,
+    VERSION integer not null,
+    CREATE_TS timestamp,
+    CREATED_BY varchar(50),
+    UPDATE_TS timestamp,
+    UPDATED_BY varchar(50),
+    DELETE_TS timestamp,
+    DELETED_BY varchar(50),
+    --
+    DIAGNOSIS_ICD10_NAME varchar(255),
+    DIAGNOSIS_NAME varchar(255),
+    CREATION_INFO_CLINIC_NAME longvarchar,
+    CREATION_INFO_DOCTOR_SURNAME varchar(255),
+    CREATION_INFO_DATE_ timestamp not null,
+    DESCRIPTION_COMPLAINTS longvarchar,
+    DESCRIPTION_ANAMNESIS longvarchar,
+    DESCRIPTION_OBJECTIVELY longvarchar,
+    DESCRIPTION_CONCLUSION longvarchar,
+    --
+    primary key (ID)
+)^
+-- end MEDICALRECORD_CONSULTATION_RECORD
