fa67c44cc59997dc87cefed11816a1ab9c4478e0
Daniil Kudiyarov <daniilkudiyarovsocial@gmail.com>
2021-07-15 14:58:40 +0400

    Add init values for NoteRecord

--- modules/core/db/init/hsql/30.create-db.sql
+++ modules/core/db/init/hsql/30.create-db.sql
@@ -1,1 +1,5 @@
-
+INSERT INTO MEDICALRECORD_NOTE_RECORD
+(ID, VERSION, CREATE_TS, CREATED_BY, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY,NAME,DESCRIPTION,DATE_)
+VALUES
+('1728d1fc-243e-0cd4-08d3-717ea8a48e12', 1, '2018-11-05 17:26:24', 'admin', '2018-11-05 17:26:24', null, null, null,'Заметка №1','Описание к заметке','2021-05-07 10:00:00'),
+('3d9c0a8d-fe96-dd9d-e4c8-43622a40288b', 2, '2018-11-05 17:38:39', 'admin', '2018-11-05 17:45:54', null , null, null,'Заметка №2','Текст заметки','2021-05-07 15:00:00');
